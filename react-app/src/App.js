5131753abf4ec383fa62e898cc72f515ec2f090c
ithemickey <31934909+ithemickey@users.noreply.github.com>
2019-01-20 21:09:09 +0530

    state,handling events and methods,passing method ref between components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,14 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/person';
 class App extends Component {
+    state={
+        persons:[
+            {name:'Alex',age:28},
+            {name:'July',age:26},
+            {name:'Stephy',age:29}
+            ],
+        otherSate:"Other value"
+    }
+    //Passing argument newName 
+    switchNameHandler=(newName) =>{
+       console.log("Button was clicked");
+       this.setState({
+            persons:[{name:newName,age:28},
+            {name:'July',age:26},
+            {name:'Stephy',age:"29"}]
+    } )
+}     
+     /*On click in line 28 is an inefficient way of paasing arguments prefer bind() insted*/
     render() {
         return (
 
             <div className = "App" >
             <h1> Heya!Great Start </h1>    
-             <Person name="Alex" age="28"/> 
-             <Person name="July" age="26">My hobbies: Skating</Person> 
-            </div>
+          
+            <button onClick={() => this.switchNameHandler('Mayank Chakka hai')}>Click me!  </button>
+             <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/> 
+             <Person click={this.switchNameHandler.bind(this,'Karen')} name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Skating</Person> 
+           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+             </div>
         );
     }
 }
--- react-app/src/Person/person.js
+++ react-app/src/Person/person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const person = (props) => {
-    return <div><p> Hi i am {props.name} , My age is {props.age}</p>
+    return <div><p onClick={props.click}> Hi i am {props.name} , My age is {props.age}</p>
     <p>{props.children}</p>
     </div>
 };
